75bb552ebb5741ed33fc02fc3d0b79786dec62b3
yuleeee <43906775+yuleeee@users.noreply.github.com>
2019-09-15 16:32:58 +0800

    Create 栈的压入弹出.cpp

---
+++ 栈的压入弹出.cpp
@@ -0,0 +1,49 @@
+/*
+题目描述
+输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。
+假设压入栈的所有数字均不相等。
+例如序列1,2,3,4,5是某栈的压入顺序，
+序列4,5,3,2,1是该压栈序列对应的一个弹出序列，
+但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）
+*/
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        int len = pushV.size();
+        stack<int> test;
+        if(len==0)
+            return true;
+        int i =0;
+        int j =0;
+        
+        for(;j<len;j++)
+        {
+            if(pushV[j]==popV[i])
+            {
+                //test.push(pushV[j]);
+                //test.pop();
+                i++;
+                continue;
+            }
+            else
+            {
+                test.push(pushV[j]);
+            }
+/*                if(j<len)test.push(pushV[j]);
+                while(!test.empty()&&test.top()==popV[i])
+                {
+                    test.pop();
+                    i++;
+                }
+            */
+        }
+        while(!test.empty()&&test.top()==popV[i])
+                {
+                    test.pop();
+                    i++;
+                }
+        if(i!=len)
+            return false;
+        return true;
+    }
+};
